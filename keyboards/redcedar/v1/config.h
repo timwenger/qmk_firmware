e7178a58a0568839a5db05a4e21b37cdcbd7bd55
Tim Wenger <timwenger@outlook.com>
2024-08-03 14:07:51 -0700

    simplify red cedar config using tested config from whitecedar

--- keyboards/redcedar/v1/config.h
+++ keyboards/redcedar/v1/config.h
@@ -15,8 +15,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#define SERIAL_USART_FULL_DUPLEX
-#define SERIAL_USART_TX_PIN GP16
-#define SERIAL_USART_RX_PIN GP17
-#define SERIAL_USART_PIN_SWAP
 
+// It may be possible for me to define these in the keyboard.json file by updating this mapping file,
+// but I haven't tried it:
+// /home/tim/qmk_firmware/data/mappings/info_config.hjson
+
+// when using the PIO driver, you can use any GPIO and define SERIAL_USART_PIN_SWAP
+// if using the SIO driver, you can only use UART capable pins, and cannot pin swap
+// https://github.com/qmk/qmk_firmware/blob/master/docs/platformdev_rp2040.md#split-keyboard-support
+// https://github.com/qmk/qmk_firmware/blob/master/docs/drivers/serial.md#usart-full-duplex
+#define SERIAL_USART_FULL_DUPLEX
+#define SERIAL_USART_TX_PIN GP21 
+#define SERIAL_USART_RX_PIN GP23
+#define SERIAL_USART_PIN_SWAP
--- keyboards/redcedar/v1/keymaps/default/keymap.c
+++ keyboards/redcedar/v1/keymaps/default/keymap.c
@@ -1,23 +1,7 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include QMK_KEYBOARD_H
-
-#define _BASE 0
-#define _RAISE 1
-#define _LOWER 2
-
-#define SFT_ESC  SFT_T(KC_ESC)
-#define CTL_BSPC CTL_T(KC_BSPC)
-#define ALT_SPC  ALT_T(KC_SPC)
-#define SFT_ENT  SFT_T(KC_ENT)
-
-#define KC_ML KC_MS_LEFT
-#define KC_MR KC_MS_RIGHT
-#define KC_MU KC_MS_UP
-#define KC_MD KC_MS_DOWN
-#define KC_MB1 KC_MS_BTN1
-#define KC_MB2 KC_MS_BTN1
-
-#define RAISE MO(_RAISE)
-#define LOWER MO(_LOWER)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base (qwerty)
@@ -28,33 +12,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
      * | SHFT |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   /  |      |
      * +------+------+------+------+-------------+                             +-------------+------+------+------+------+
-     *               |  [   |   ]  | 0    |                                           | 5    |  +   |  =   |
+     *               |  [   |   ]  | a    |                                           | 5    |  +   |  =   |
      *               +-------------+-------------+                             +-------------+-------------+
-     *                             |  1   |   2  |                             |  3   |  4   |
+     *                             |  b   |   c  |                             |  3   |  4   |
      *                             |------+------|                             |------+------|
 
      */
-    [_BASE] = LAYOUT(
+    [0] = LAYOUT(
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                          KC_LBRC, KC_RBRC, KC_A,                                KC_P5,   KC_PLUS, KC_EQL,
-                                            KC_B,   KC_C,              KC_P3,   KC_P4
-    ),
-
-    [_LOWER] = LAYOUT(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                          KC_LBRC, KC_RBRC, KC_P0,                                KC_P5,   KC_PLUS, KC_EQL,
-                                            KC_P1,   KC_P2,              KC_P3,   KC_P4
-    ),
-
-    [_RAISE] = LAYOUT(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                          KC_LBRC, KC_RBRC, KC_P0,                                KC_P5,   KC_PLUS, KC_EQL,
-                                            KC_P1,   KC_P2,              KC_P3,   KC_P4
+                          KC_LBRC, KC_RBRC, KC_A,                                 KC_P5,   KC_PLUS, KC_EQL,
+                                            KC_B,   KC_C,                KC_P3,   KC_P4
     )
 };
