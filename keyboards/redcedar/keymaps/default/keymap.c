68d2e1a6bed45aeadb46719f7c5920d0be39348e
Tim Wenger <timwenger@outlook.com>
2024-12-28 11:54:27 -0800

    added layers, got shift-backspace for delete working

--- keyboards/redcedar/keymaps/default/keymap.c
+++ keyboards/redcedar/keymaps/default/keymap.c
@@ -5,6 +5,12 @@
 # include "print.h"
 # include "g/keymap_combo.h"
 
+enum layer_names {
+  HOME,
+  AROW,
+  NUM
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base (qwerty)
      * +-----------------------------------------+                             +-----------------------------------------+
@@ -20,56 +26,75 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                             +------+------+                             +------+------+
 
      */
-    [0] = LAYOUT(
-        QK_BOOTLOADER,  KC_Q,    KC_W,    KC_F,    KC_P,             KC_G,               KC_J,            KC_L,    KC_U,    KC_Y,    KC_ESC,  QK_BOOTLOADER,
-        KC_TAB,         KC_A,    KC_R,    KC_S,    KC_T,             KC_D,               KC_H,            KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
-        KC_NO,          KC_Z,    KC_X,    KC_C,    KC_V,             KC_B,               KC_K,            KC_M,    KC_COMM, KC_DOT,  KC_QUES, KC_NO,
-                                 KC_NO,   KC_NO,   LSFT_T(KC_BSPC),  KC_NO,              RSFT_T(KC_SPC),  KC_BSLS, KC_SLSH, KC_NO,
-                                                   KC_LALT,          KC_LCTL,            KC_RCTL,         KC_RALT
+    [HOME] = LAYOUT(
+        QK_BOOTLOADER,  KC_Q,           KC_W,     KC_F,     KC_P,             KC_G,             KC_J,            KC_L,    KC_U,    KC_Y,    KC_ESC,          QK_BOOTLOADER,
+        KC_TAB,         LT(NUM, KC_A),  KC_R,     KC_S,     KC_T,             KC_D,             KC_H,            KC_N,    KC_E,    KC_I,    LT(AROW, KC_O),  KC_ENT,
+        KC_NO,          KC_Z,           KC_X,     KC_C,     KC_V,             KC_B,             KC_K,            KC_M,    KC_COMM, KC_DOT,  KC_QUES,         KC_NO,
+                                        KC_BSLS,  KC_PIPE,  LSFT_T(KC_BSPC),  KC_NO,            RSFT_T(KC_SPC),  KC_NO,   KC_NO,   KC_NO,
+                                                            KC_LALT,          KC_LCTL,          KC_RCTL,         KC_RALT
+    ),
+    [AROW] = LAYOUT(
+        KC_NO,          KC_NO,          KC_NO,    KC_UP,    KC_NO,            KC_NO,            KC_F12,           KC_F7,  KC_F8,   KC_F9,   KC_NO,           KC_NO,
+        KC_NO,          KC_HOME,        KC_LEFT,  KC_DOWN,  KC_RGHT,          KC_END,           KC_F11,           KC_F4,  KC_F5,   KC_F6,   KC_NO,           KC_NO,
+        KC_NO,          KC_NO,          KC_NO,    KC_NO,    KC_NO,            KC_NO,            KC_F10,           KC_F1,  KC_F2,   KC_F3,   KC_NO,           KC_NO,
+                                        KC_NO,    KC_NO,    LSFT_T(KC_BSPC),  KC_NO,            RSFT_T(KC_SPC),   KC_NO,  KC_NO,   KC_NO,
+                                                            KC_LALT,          KC_LCTL,          KC_RCTL,          KC_RALT
+    ),
+    [NUM] = LAYOUT(
+        KC_NO,          KC_NO,          KC_NO,    KC_NO,    KC_NO,            KC_NO,            KC_F12,           KC_7,   KC_8,    KC_9,   KC_NO,           KC_NO,
+        KC_NO,          KC_NO,          KC_NO,    KC_NO,    KC_NO,            KC_NO,            KC_F11,           KC_4,   KC_5,    KC_6,   KC_KP_EQUAL,     KC_NO,
+        KC_NO,          KC_NO,          KC_NO,    KC_NO,    KC_NO,            KC_NO,            KC_F10,           KC_1,   KC_2,    KC_3,   KC_NO,           KC_NO,
+                                        KC_NO,    KC_NO,    LSFT_T(KC_BSPC),  KC_NO,            RSFT_T(KC_SPC),   KC_0,   KC_DOT,  KC_NO,
+                                                            KC_LALT,          KC_LCTL,          KC_RCTL,          KC_RALT
     )
 };
 
+uint8_t mod_state;
+bool process_record_user(uint16_t keycode, keyrecord_t *record)
+{
+  mod_state = get_mods();
 
-// bool process_record_user(uint16_t keycode, keyrecord_t *record)
-// {
-//   // https://www.monotux.tech/posts/2024/09/qmk-shift-backspace-delete/
+  switch(keycode) { 
+    // Switch backspace for a delete press, if holding shift
+    // https://github.com/qmk/qmk_firmware/blob/master/docs/feature_advanced_keycodes.md#shift--backspace-for-delete-shift-backspace-for-delete
+    // This case, LSFT_T(KC_BSPC), has to capture the whole feature code of the
+    // left thumb press, not just KC_BSPC
+    case LSFT_T(KC_BSPC): {
+      static bool delkey_registered;
 
-//   switch(keycode) { // switch backspace for a delete press, if holding shift
-//     case KC_BSPC: {
-//       static uint16_t registered_key = KC_NO;
-//       if (record->event.pressed) {  // On key press.
-//         const uint8_t mods = get_mods();
-// #ifndef NO_ACTION_ONESHOT
-//         uint8_t shift_mods = (mods | get_oneshot_mods()) & MOD_MASK_SHIFT;
-// #else
-//         uint8_t shift_mods = mods & MOD_MASK_SHIFT;
-// #endif  // NO_ACTION_ONESHOT
-//         if (shift_mods) {  // At least one shift key is held.
-//           registered_key = KC_DEL;
-//           // If one shift is held, clear it from the mods. But if both
-//           // shifts are held, leave as is to send Shift + Del.
-//           if (shift_mods != MOD_MASK_SHIFT) {
-// #ifndef NO_ACTION_ONESHOT
-//             del_oneshot_mods(MOD_MASK_SHIFT);
-// #endif  // NO_ACTION_ONESHOT
-//             unregister_mods(MOD_MASK_SHIFT);
-//           }
-//         } else {
-//           registered_key = KC_BSPC;
-//         }
+      if (record->event.pressed) {  // On key press.
+        if (keyboard_report->mods & MOD_BIT(KC_RSFT)) {
+          // First temporarily canceling shift so that
+          // shift isn't applied to the KC_DEL keycode
+          del_mods(MOD_MASK_SHIFT);
+          register_code(KC_DEL);
 
-//         register_code(registered_key);
-//         set_mods(mods);
-//       } else {  // On key release.
-//         unregister_code(registered_key);
-//       }
-//     } return false;
+          // Update the boolean variable to reflect the status of KC_DEL
+          delkey_registered = true;
+
+          // Reapplying modifier state so that the held shift key
+          // still works even after having tapped the Backspace/Delete key.
+          set_mods(mod_state);
+          return false;
+        }
+        // if the right shift wasn't pressed, return true and do the normal LSFT_T(KC_BSPC)
+        return true;
+
+      } else { // on release of LSFT_T(KC_BSPC)
+        // In case KC_DEL is still being sent even after the release of LSFT_T(KC_BSPC)
+        if (delkey_registered) {
+          unregister_code(KC_DEL);
+          delkey_registered = false;
+          return false;
+        }
+      }
+    }
+  };
 
 
-//   };
 
-//   return true;
-// }
+  return true;
+}
 
 // int count = 0;
 // report_mouse_t pointing_device_task_user(report_mouse_t left_report) {
