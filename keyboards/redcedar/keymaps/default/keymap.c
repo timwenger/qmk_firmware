1119411159d077774cffab69cbe2e4e6d14e2d79
Tim Wenger <timwenger@outlook.com>
2025-01-04 14:13:59 -0800

    moved ENTER and DEL to the upper layers. So now I get to them by pressing either pinky, rather than holding the other shift key

--- keyboards/redcedar/keymaps/default/keymap.c
+++ keyboards/redcedar/keymaps/default/keymap.c
@@ -32,26 +32,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,          KC_NO,          KC_NO,    KC_UP,    KC_NO,            KC_NO,            KC_F12,    KC_F7,            KC_F8,   KC_F9,   KC_NO,           KC_NO,
         KC_NO,          KC_HOME,        KC_LEFT,  KC_DOWN,  KC_RGHT,          KC_END,           KC_F11,    KC_F4,            KC_F5,   KC_F6,   KC_TRNS,         KC_NO,
         KC_NO,          KC_NO,          KC_NO,    KC_NO,    KC_NO,            KC_NO,            KC_F10,    KC_F1,            KC_F2,   KC_F3,   KC_NO,           KC_NO,
-                                        KC_LALT,  KC_TAB,   LSFT_T(KC_BSPC),                               RSFT_T(KC_SPC),   KC_NO,  KC_NO,
+                                        KC_LALT,  KC_TAB,   LSFT_T(KC_DEL),                                RSFT_T(KC_ENTER), KC_NO,  KC_NO,
                                                             KC_LALT,          KC_LCTL,          KC_RCTL,   KC_RALT
     ),
     [NUM] = LAYOUT(
         KC_NO,          KC_NO,          KC_NO,    KC_NO,    KC_NO,            KC_NO,            KC_NO,     KC_7,             KC_8,   KC_9,   KC_NO,           KC_NO,
         KC_NO,          KC_TRNS,        KC_PIPE,  KC_BSLS,  KC_SLSH,          KC_NO,            KC_PPLS,   KC_4,             KC_5,   KC_6,   KC_0,            KC_NO,
         KC_NO,          KC_NO,          KC_NO,    KC_NO,    KC_NO,            KC_NO,            KC_NO,     KC_1,             KC_2,   KC_3,   KC_NO,           KC_NO,
-                                        KC_LALT,  KC_TAB,   LSFT_T(KC_BSPC),                               RSFT_T(KC_SPC),   KC_COMM, KC_DOT,
+                                        KC_LALT,  KC_TAB,   LSFT_T(KC_DEL),                                RSFT_T(KC_ENTER),   KC_COMM, KC_DOT,
                                                             KC_LALT,          KC_LCTL,          KC_RCTL,   KC_RALT
     )
 };
 
-const key_override_t shift_backspace_delete = ko_make_basic(MOD_MASK_SHIFT, LSFT_T(KC_BSPC), KC_DEL);
-const key_override_t shift_space_enter = ko_make_basic(MOD_MASK_SHIFT, RSFT_T(KC_SPC), KC_ENTER);
+// const key_override_t shift_backspace_delete = ko_make_basic(MOD_MASK_SHIFT, LSFT_T(KC_BSPC), KC_DEL);
+// const key_override_t shift_space_enter = ko_make_basic(MOD_MASK_SHIFT, RSFT_T(KC_SPC), KC_ENTER);
 const key_override_t exclamation_mark = ko_make_basic(MOD_MASK_SHIFT, KC_QUES, KC_EXCLAIM);
 
 // This globally defines all key overrides to be used
 const key_override_t *key_overrides[] = {
-	&shift_backspace_delete,
-  &shift_space_enter,
+	// &shift_backspace_delete,
+  // &shift_space_enter,
   &exclamation_mark
 };
 
