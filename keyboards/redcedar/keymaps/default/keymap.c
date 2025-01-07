0e5dabe79adb05d9887a12ada76f256df41a76e3
Tim Wenger <timwenger@outlook.com>
2025-01-07 00:39:26 -0800

    use thumbs for combos for clicking and - and =. But also gave - and = their own keys. I think I'll like that better, even if they aren't in convenient locations

--- keyboards/redcedar/keymaps/default/keymap.c
+++ keyboards/redcedar/keymaps/default/keymap.c
@@ -36,8 +36,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                             KC_LALT,          KC_LCTL,          KC_RCTL,   KC_RALT
     ),
     [NUM] = LAYOUT(
-        KC_NO,          KC_NO,          KC_NO,    KC_NO,    KC_NO,            KC_NO,            KC_NO,     KC_7,             KC_8,   KC_9,   KC_NO,           KC_NO,
-        KC_NO,          KC_TRNS,        KC_PIPE,  KC_BSLS,  KC_SLSH,          KC_NO,            KC_PPLS,   KC_4,             KC_5,   KC_6,   KC_0,            KC_NO,
+        KC_NO,          KC_NO,          KC_NO,    KC_NO,    KC_NO,            KC_NO,            KC_EQL,    KC_7,             KC_8,   KC_9,   KC_NO,           KC_NO,
+        KC_NO,          KC_TRNS,        KC_PIPE,  KC_BSLS,  KC_SLSH,          KC_MINS,          KC_PPLS,   KC_4,             KC_5,   KC_6,   KC_0,            KC_NO,
         KC_NO,          KC_NO,          KC_NO,    KC_NO,    KC_NO,            KC_NO,            KC_NO,     KC_1,             KC_2,   KC_3,   KC_NO,           KC_NO,
                                         KC_LALT,  KC_TAB,   LSFT_T(KC_BSPC),                               RSFT_T(KC_SPC),   KC_COMM, KC_DOT,
                                                             KC_LALT,          KC_LCTL,          KC_RCTL,   KC_RALT
