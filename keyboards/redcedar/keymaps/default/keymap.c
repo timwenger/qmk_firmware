c9eaf48bb99bc75d0414d16a5a361688513a74cd
Tim Wenger <timwenger@outlook.com>
2025-01-09 17:48:49 -0800

    move delete mod to nums layer, so I can delete files with one hand while using the mouse. This means I can't use backspace when entering numbers, but I think it's worth it.

--- keyboards/redcedar/keymaps/default/keymap.c
+++ keyboards/redcedar/keymaps/default/keymap.c
@@ -32,14 +32,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,          KC_NO, QK_MOUSE_BUTTON_2, KC_UP,   QK_MOUSE_BUTTON_1, KC_NO,            KC_F12,    KC_F7,            KC_F8,   KC_F9,   KC_NO,           KC_NO,
         KC_NO,          KC_HOME,        KC_LEFT,  KC_DOWN,  KC_RGHT,          KC_END,           KC_F11,    KC_F4,            KC_F5,   KC_F6,   KC_TRNS,         KC_NO,
         KC_NO,          LCTL(KC_Z),   LCTL(KC_X), LCTL(KC_C), LCTL(KC_V),     KC_NO,            KC_F10,    KC_F1,            KC_F2,   KC_F3,   KC_NO,           KC_NO,
-                                        KC_LALT,  KC_TAB,   LSFT_T(KC_DEL),                                RSFT_T(KC_ENTER), KC_NO,  KC_NO,
+                                        KC_LALT,  KC_TAB,   LSFT_T(KC_BSPC),                               RSFT_T(KC_ENTER), KC_NO,  KC_NO,
                                                             KC_LALT,          KC_LCTL,          KC_RCTL,   KC_RALT
     ),
     [NUM] = LAYOUT(
         KC_NO,          KC_NO, QK_MOUSE_BUTTON_2, KC_NO,   QK_MOUSE_BUTTON_1, KC_NO,            KC_EQL,    KC_7,             KC_8,   KC_9,   KC_NO,           KC_NO,
         KC_NO,          KC_TRNS,        KC_PIPE,  KC_BSLS,  KC_SLSH,          KC_MINS,          KC_PPLS,   KC_4,             KC_5,   KC_6,   KC_0,            KC_NO,
         KC_NO,          KC_NO,          KC_NO,    KC_NO,    KC_NO,            KC_NO,            KC_NO,     KC_1,             KC_2,   KC_3,   KC_NO,           KC_NO,
-                                        KC_LALT,  KC_TAB,   LSFT_T(KC_BSPC),                               RSFT_T(KC_SPC),   KC_COMM, KC_DOT,
+                                        KC_LALT,  KC_TAB,   LSFT_T(KC_DEL),                                RSFT_T(KC_SPC),   KC_COMM, KC_DOT,
                                                             KC_LALT,          KC_LCTL,          KC_RCTL,   KC_RALT
     )
 };
